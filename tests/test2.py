3933c85d8a29b1270c0c34e9706db453d76c338d
Tanay Rangasamy <2544162@students.wits.ac.za>
2025-04-07 18:04:35 +0200

    Added comments to 2nd test explaining code

--- tests/test2.py
+++ tests/test2.py
@@ -2,7 +2,6 @@ print("Test 2")
 import unittest
 import code
 import io
-import code.getbest
 
 class TestFindTop(unittest.TestCase):
    # def setUp(self):
@@ -11,14 +10,14 @@ class TestFindTop(unittest.TestCase):
 
     def test_Functio(self):
         print("In testing function")
-        number_col =1
+        number_col =1  #Set the indexes that getCol would've gotten
         mark_col = 2
-        ColData = "ELEN3020,160001,72,OK\nELEN3020,167381,90,Check\n"
+        ColData = "ELEN3020,160001,72,OK\nELEN3020,167381,90,Check\n" #Simple test string
         f = io.StringIO(ColData)
         num_index = 0
         mark = 0
-        num_index, mark = code.getbest.findTop(f,number_col,mark_col)
-        self.assertEqual((num_index,mark),('167381',90))
+        num_index, mark = code.getbest.findTop(f,number_col,mark_col) #Run the findTop function
+        self.assertEqual((num_index,mark),('167381',90)) #See if the function returns the correct expected value
 
 if __name__=='__main__':
     unittest.main()
