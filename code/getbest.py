8ae7ba65e916e176a7eca5c61c9d353973c7abf6
Tanay Rangasamy <2544162@students.wits.ac.za>
2025-04-07 17:36:55 +0200

    Added 1st test

--- code/getbest.py
+++ code/getbest.py
@@ -6,6 +6,8 @@ def getCols(f):
     ''' Identify the columns that contain the marks and student numbers '''
     headings = f.readline().strip().split(",")
     i=0
+    num_col = -1
+    mark_col = -1
     for head in headings:
         if head == "Student Number": num_col=i
         elif head == "Mark" : mark_col = i
@@ -24,7 +26,8 @@ def findTop(f,num_col, mark_col):
             best_idx = data[num_col]
     return best_idx, best
 
-f = open(sys.argv[1])
-num_col, mark_col = getCols(f)
-best_idx, best = findTop(f,num_col,mark_col)
-print("The top student was student %s with %d"%(best_idx,best))
+if __name__ == '__main__':   #Seen in stratify lab example file for tests
+    f = open(sys.argv[1])
+    num_col, mark_col = getCols(f)
+    best_idx, best = findTop(f,num_col,mark_col)
+    print("The top student was student %s with %d"%(best_idx,best))
--- tests/test1.py
+++ tests/test1.py
@@ -2,17 +2,19 @@ print("Test One")
 import unittest
 import sys
 import os
-import io
-from code import getbest
+import io 
+import code #Folder containing getbest.py, the __init__.py file has details on the import
 
 class TestColumnMethod(unittest.TestCase):
     def test_col1(self):
         print("Testing columns")
-        number_col2 = 2
-        mark_col2 = 3
+        number_col2 = -1
+        mark_col2 = -1
         ColData = "Course,Student Number,Mark,Comment"
-        number_col2, mark_col2 = getbest.getCols(ColData)
-        self.assertEqual((number_col2,mark_col2),(2,3))
+       # f = open(sys.argv[1])
+        f = io.StringIO(ColData) #Puts test string in a csv file-type format
+        number_col2, mark_col2 = code.getbest.getCols(f) #store the results of the function from the test string
+        self.assertEqual((number_col2,mark_col2),(1,2)) #Compare the results to expected results
 
 if __name__ == '__main__':
     unittest.main()
